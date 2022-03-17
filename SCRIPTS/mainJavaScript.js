07ca88945335240cb51ad00c98c66cee77677e92
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-03-17 14:16:35 +0200

    added calculator and style
    added the js calculator and did styleing around calculator

--- SCRIPTS/mainJavaScript.js
+++ SCRIPTS/mainJavaScript.js
@@ -3,3 +3,23 @@ myName = prompt('What is your Name');
 
 let greetingMessage;
 greetingMessage = document.querySelector('h1').textContent = "We welcome you, " + myName + ", to CALCULATE-X, the simplest online calculator.";
+
+function displayCalculator(val){
+    document.getElementById('result').value += val;
+
+    return val;
+};
+
+function solve(){
+    let x = document.getElementById('result').value;
+
+    let y = eval(x);
+
+    document.getElementById('result').value = y;
+
+    return y;
+};
+
+function clearScreen(){
+    document.getElementById('result').value = "";
+}
