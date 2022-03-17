634b984a65d756a4c9d97b40b489040319f204d4
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-03-17 12:31:47 +0200

    basic layout set
    basic layout and color-scheme set

---
+++ SCRIPTS/mainJavaScript.js
@@ -0,0 +1,5 @@
+let myName;
+myName = prompt('What is your Name');
+
+let greetingMessage;
+greetingMessage = document.querySelector('h1').textContent = "We welcome you, " + myName + ", to CALCULATE-X, the simplest online calculator.";
